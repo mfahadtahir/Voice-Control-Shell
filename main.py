1a947feb936ec5f8bed020c7836eed1283d903e8
mfahadtahir <muhammadfahad325302@gmail.com>
2021-01-23 20:25:38 +0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,114 @@
+from time import sleep
+import sys
+import formatting
+import voice
+import threading
+import modules
+import subprocess
+
+
+def welcome_message(msg):
+  voice.text_to_speech(msg)
+
+def welcome_heading():
+    formatting.line(86)
+    sleep(0.3)
+    for x in '| Hi, I am your Virtual Assistant and you can ask me to perform the following tasks. |\n':
+        print(x, end='')
+        sys.stdout.flush()
+        sleep(0.07)
+    
+    formatting.line(86)
+    return
+  
+def welcome_window():
+    subprocess.call(['clear'])
+    formatting.head('✨ Voice Controlled Shell ✨')
+    
+    # Making a thread to synchronize the audio message and text message
+    writingText = threading.Thread(target=welcome_message, name="Welcome Audio", args=('Hi, I am your virtual Assistant and you can ask me to perform the following tasks.',))
+    writingText.start()
+    welcome_heading()    
+    return
+
+def execute(mytext):
+    if(mytext.lower() == 'send email'):
+        modules.send_mail()
+        return
+    elif(mytext.lower() == 'weather update'):
+        modules.weather_update()
+        return
+    elif(mytext.lower() == 'list files'):
+        modules.list_files()
+        return
+    elif(mytext.lower() == 'date'):
+        modules.today_date()
+        return
+    elif(mytext.lower() == 'shutdown'):
+        modules.shutdown_now()
+        return
+    elif(mytext.lower() == 'reboot'):
+        modules.reboot_now()
+        return
+    elif(mytext.lower() == 'create file'):
+        modules.create_file()
+        return
+    elif(mytext.lower() == 'file type'):
+        modules.file_type()
+        return
+    elif(mytext.lower() == 'create user'):
+        modules.create_user()
+        return
+    elif(mytext.lower() == 'delete user'):
+        modules.delete_user()
+        return
+    elif(mytext.lower() == 'current directory'):
+        modules.current_working_dir()
+        return
+    elif(mytext.lower() == 'create directory'):
+        modules.create_dir()
+        return
+    elif(mytext.lower() == 'go back'):
+        modules.back()
+        return
+    elif(mytext.lower() == 'change directory'):
+        modules.change_dir()
+        return
+    elif(mytext.lower() == 'delete file'):
+        modules.delete_file()
+        return
+    elif(mytext.lower() == 'delete directory'):
+        modules.delete_dir()
+        return
+    else:
+        formatting.text_box('Invalid Command!')
+        return
+
+if __name__ == '__main__':
+    flag = 1
+    welcome_window()
+    input('Press Enter to continue...')
+    subprocess.call(['clear'])
+    while 1:
+        # 
+        if(flag == 1):
+          mytext = voice.speech_to_text(1)
+          flag = 0
+        else:
+            mytext = voice.speech_to_text()
+        
+        # Checking Errors 
+        if(voice.error_status(mytext)):
+          formatting.text_box('Please, try again!')
+          continue
+        # Checking exit
+        elif(mytext == 'exit'):
+          formatting.text_box('Good Bye!')
+          voice.text_to_speech('Good Bye!')
+          exit()
+        # calling the functionality 
+        else:
+          subprocess.call(['clear'])
+          execute(mytext)
+          input('Press Enter to continue...')
+          subprocess.call(['clear'])
---
+++ voice.py
@@ -0,0 +1,47 @@
+import speech_recognition as sr
+from gtts import gTTS
+import formatting
+import subprocess
+import os
+import formatting
+#this file contains input output functions
+
+
+path = os.getcwd()
+
+#this function will tell if the error occur or not
+def error_status(mytext):
+    if(mytext == 0):
+        return True
+    else:
+        return False
+
+#this function converts speech to text
+def speech_to_text(flag = 0):
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        if(flag == 1):
+            subprocess.call(['clear'])
+            formatting.show_list()
+        formatting.text_box('Listening...')
+        audio = r.listen(source)
+    try:
+        mytext = r.recognize_google(audio)
+        return mytext
+    except sr.UnknownValueError:
+        formatting.text_box('Google Speech Recognition could not understand audio')
+        return 0
+    except sr.RequestError as e:
+        formatting.text_box('Could not request results from Google Speech Recognition service; {0}'.format(e))
+        return 0
+
+#this function converts text to speech
+def text_to_speech(mytext):
+    global path
+    if(error_status(mytext)):
+        return
+    else:
+        myObj = gTTS(text = mytext, lang = 'en')
+        myObj.save(path + '/main.mp3')
+        os.system('mpg321 -q ' + path + '/main.mp3')
+        return
